06a7ca4c70c30d8080f314cecfb1a4cfc2fce234
Karim Abou Zeid <karim23697@gmail.com>
2017-10-08 18:35:07 +0200

    Move installer scripts to root dir

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Link the dotfiles
 echo "Linking dotfiles"
-source install/link.sh -cinstall/link.conf "$@"
+source link.sh "$@"
 
 # Install vim plugins
 echo "Installing vim plugins"
@@ -15,6 +15,6 @@ vim -c "execute \"PlugInstall!\" | qa"
 # perform macOS specific install
 if [ "$(uname)" == "Darwin" ]; then
     echo "Installing macOS specific stuff"
-    source install/brew.sh
-    source install/osx.sh
+    source brew.sh
+    source osx.sh
 fi
