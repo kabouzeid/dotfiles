7f71bfb04e25d3fcc6ab16c64853986cb8eae99a
Karim Abou Zeid <karim23697@gmail.com>
2017-11-20 00:01:55 +0100

    Add cask installer

---
+++ cask.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+brew tap caskroom/cask
+brew update
+
+echo "Installing Homebrew Casks"
+
+brew cask install android-ndk
+brew cask install android-sdk
+brew cask install atom
+brew cask install betterzip
+brew cask install google-chrome
+brew cask install handshaker
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install jd-gui
+brew cask install karabiner-elements
+brew cask install logitech-options
+brew cask install macvim
+brew cask install osxfuse
+brew cask install qlcolorcode
+brew cask install qlmarkdown
+brew cask install qlvideo
+brew cask install shimo
+brew cask install spotify
+brew cask install sync-my-l2p
+brew cask install telegram
+brew cask install torbrowser
+brew cask install ukelele
+brew cask install unetbootin
+brew cask install vlc
+brew cask install whatsapp
+brew cask install wireshark
+brew cask install xquartz
