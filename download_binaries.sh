ff34816ef2831a2b995edaa24e62a9cc1415ff98
Karim Abou Zeid <karim23697@gmail.com>
2021-10-13 17:09:33 +0200

    node binary

--- download_binaries.sh
+++ download_binaries.sh
@@ -16,3 +16,8 @@ ln -s delta-0.8.3-x86_64-unknown-linux-gnu/delta .
 curl -L -o gh.tar.gz "https://github.com/cli/cli/releases/download/v2.0.0/gh_2.0.0_linux_amd64.tar.gz"
 tar -xzf gh.tar.gz
 ln -s gh_2.0.0_linux_amd64/bin/gh .
+
+# node
+curl -L -o node.tar.xz "https://nodejs.org/dist/v16.11.1/node-v16.11.1-linux-x64.tar.xz"
+tar -xf node.tar.xz
+ln -sf node-v16.11.1-linux-x64/bin/* .
