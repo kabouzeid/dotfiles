f8d57225ca9e4e0e18fab5df5f8117c65a77bde6
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2023-04-19 21:23:31 +0200

    updates

---
+++ config/fish/conf.d/asdf.fish
@@ -0,0 +1,14 @@
+# source asdf if it's installed
+if type -q brew
+  source /opt/homebrew/opt/asdf/libexec/asdf.fish
+else if test -d "$HOME/.asdf"
+  source "$HOME/.asdf/asdf.fish"
+end
+
+# since we'll be using asdf-direnv, remove shims dir from `$fish_user_paths`
+if test -n "$ASDF_DATA_DIR"
+    set -l SHIMS_DIR "$ASDF_DATA_DIR/shims"
+else
+    set -l SHIMS_DIR "$HOME/.asdf/shims"
+end
+set fish_user_paths (string match --invert $fish_user_paths)
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,8 @@
-test -e "$HOME/.config/fish/config.local.pre.fish"; and source "$HOME/.config/fish/config.local.pre.fish"
+# put your local config in ./conf.d/0.local.fish
+
+fish_add_path --global "$HOME/.local/bin" "$HOME/.cargo/bin"
 
 if status is-login
-    fish_add_path --global "$HOME/.local/bin" "$HOME/.cargo/bin"
-
     if type -q nvim
         set -gx EDITOR nvim
     else
@@ -16,20 +16,17 @@ if status is-login
 end
 
 if status is-interactive
-    alias v $EDITOR
-
     test -e "$HOME/.iterm2_shell_integration.fish" ; and source "$HOME/.iterm2_shell_integration.fish"
 
-    type -q zoxide; and zoxide init fish | source
-
+    alias v $EDITOR
     type -q nvr; and set -q NVIM; and alias v "nvr --remote-tab --nostart"
     type -q xdg-open; and type -q setsid; and alias open="setsid xdg-open"
     type -q lazygit; and alias lzg lazygit
+
     type -q hx; and function hx
         PATH="$HOME/.local/share/nvim/mason/bin:$PATH" command hx $argv
     end
 
-    starship init fish | source
+    type -q zoxide; and zoxide init fish | source
+    type -q starship; and starship init fish | source
 end
-
-test -e "$HOME/.config/fish/config.local.post.fish"; and source "$HOME/.config/fish/config.local.post.fish"
