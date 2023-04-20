e1dd5ffd5652e18e968ff03950c43bcbf6b3905b
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2023-04-20 13:24:37 +0200

    updates

--- config/fish/conf.d/asdf.fish
+++ config/fish/conf.d/asdf.fish
@@ -5,11 +5,26 @@ else if test -d "$HOME/.asdf"
   source "$HOME/.asdf/asdf.fish"
 end
 
-# since we'll be using asdf-direnv, remove shims dir from `$fish_user_paths`
-set -l SHIMS_DIR
-if test -n "$ASDF_DATA_DIR"
-    set SHIMS_DIR "$ASDF_DATA_DIR/shims"
-else
-    set SHIMS_DIR "$HOME/.asdf/shims"
+if type -q asdf
+  # since we'll be using asdf-direnv, remove shims dir from `$fish_user_paths`
+  set -l SHIMS_DIR
+  if test -n "$ASDF_DATA_DIR"
+      set SHIMS_DIR "$ASDF_DATA_DIR/shims"
+  else
+      set SHIMS_DIR "$HOME/.asdf/shims"
+  end
+  set fish_user_paths (string match --invert $SHIMS_DIR $fish_user_paths)
+
+  function usas
+    echo "use asdf" >? .envrc
+    direnv allow
+  end
+
+  function usaspy
+    echo "\
+    use asdf
+    layout python
+    " >? .envrc
+    direnv allow
+  end
 end
-set fish_user_paths (string match --invert $SHIMS_DIR $fish_user_paths)
