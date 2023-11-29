8361f997cf4163e5e3f1a4f5ed67997ce0abb0ab
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2023-11-29 21:19:56 +0100

    updates

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -44,5 +44,5 @@ require("nvim-treesitter.configs").setup({
   },
   highlight = { enable = true },
   indent = { enable = true },
-  context_commentstring = { enable = true },
+  ts_context_commentstring = { enable = true },
 })
