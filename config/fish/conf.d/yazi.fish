4360bf4b61ce5e057bd8c5fe7177af108347fa84
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2024-07-27 11:43:18 +0200

    update yazi config

---
+++ config/fish/conf.d/yazi.fish
@@ -0,0 +1,10 @@
+if type -q yazi
+    function yy
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+end
