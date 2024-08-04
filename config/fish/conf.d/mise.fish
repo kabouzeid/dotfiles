25c60371469d30e7e4430422bfdd10bf5e2fc158
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2024-08-04 16:04:16 +0200

    simplify venv function

--- config/fish/conf.d/mise.fish
+++ config/fish/conf.d/mise.fish
@@ -1,23 +1,29 @@
 if type -q mise
-  if status is-interactive
-    mise activate fish | source
-  else
-    mise activate fish --shims | source
-  end
-
-  if type -q uv; and type -q sd
-    function venv
-      if test -e .mise.toml
-        sd '_.python.venv.*\n' "" .mise.toml
-      end
-
-      mise x python@$argv -- uv venv
-
-      set placeholder __PYTHON_VENV_PLACEHOLDER
-      mise set "$placeholder"=PLACEHOLDER
-      sd "$placeholder.*" '_.python.venv = ".venv"' .mise.toml
-
-      mise trust -q
+    if status is-interactive
+        mise activate fish | source
+    else
+        mise activate fish --shims | source
     end
-  end
+
+    if type -q uv; and type -q sd
+        function venv
+            if count $argv >/dev/null
+                uv venv --python $argv
+            else
+                uv venv
+            end
+
+            # remove existing venv entry
+            if test -e .mise.toml
+                sd '_.python.venv.*\n' "" .mise.toml
+            end
+
+            # add new venv entry
+            set placeholder __PYTHON_VENV_PLACEHOLDER
+            mise set "$placeholder"=PLACEHOLDER
+            sd "$placeholder.*" '_.python.venv = ".venv"' .mise.toml
+
+            mise trust -q
+        end
+    end
 end
