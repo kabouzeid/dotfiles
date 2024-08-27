a2467e27953f858dfcd2594b3b2c6ffb7568082d
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2024-08-27 13:17:58 +0200

    switch back to nvim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,7 @@
 # put your local config in ./conf.d/0-.local.fish
 
 if status is-login
-    if type -q hx
-        set -gx EDITOR hx
-    else if type -q nvim
+    if type -q nvim
         set -gx EDITOR nvim
     else
         set -gx EDITOR vim
