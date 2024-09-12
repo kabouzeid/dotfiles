46cd584f705df8afbe9893f7436fa9f52561e4be
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2024-09-12 20:16:34 +0200

    updates

--- config/fish/conf.d/uv.fish
+++ config/fish/conf.d/uv.fish
@@ -3,8 +3,9 @@ if type -q uv
     abbr pir 'uv pip install -r requirements.txt'
     abbr pil 'uv pip list'
 
-    abbr uad 'uv add'
-    abbr uar 'uv add -r requirements.txt'
+    abbr uvr 'uv run'
+    abbr uva 'uv add'
+    abbr uvar 'uv add -r requirements.txt'
 
     abbr python 'uv run python'
 end
