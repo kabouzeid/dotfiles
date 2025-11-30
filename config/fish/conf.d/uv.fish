f837877abb2ce752d31d8fbb6ca0977c9985a79c
Karim Abou Zeid <contact@ka.codes>
2025-11-30 12:11:19 +0100

    uv

--- config/fish/conf.d/uv.fish
+++ config/fish/conf.d/uv.fish
@@ -1,13 +1,12 @@
 if type -q uv
     abbr pip 'uv pip'
     abbr pir 'uv pip install -r requirements.txt'
-    abbr pil 'uv pip list'
 
     abbr uvr 'uv run'
     abbr uva 'uv add'
     abbr uvar 'uv add -r requirements.txt'
 
-    abbr uvs 'uv run $SHELL'
+    abbr uvs 'uv sync --all-extras --dev'
 
     abbr python 'uv run python'
     abbr debugpy 'uv run debugpy'
